fc83e7b455f414daf2c947e80e96042210148b0e
takashiraki <stgtbike4649@gmail.com>
2026-03-08 23:06:06 +0000

    wip

--- src/pages/friends/index.vue
+++ src/pages/friends/index.vue
@@ -1,9 +1,39 @@
 <script setup lang="ts">
 import FriendCard from '@/components/Friends/Card/FriendCard.vue';
 import { SidebarTrigger } from '@/components/ui/sidebar';
-import { ref } from 'vue';
+import { onMounted, onUnmounted, ref } from 'vue';
 
 const count = ref<number>(10);
+
+const sentinel = ref<HTMLDivElement | null>(null);
+
+let observer: IntersectionObserver | null = null;
+
+const setUpObserver = () => {
+    if (!sentinel.value) return;
+
+    observer?.disconnect();
+
+    observer = new IntersectionObserver((entries) => {
+        if (entries[0]?.isIntersecting) {
+            setTimeout(() => {
+                count.value += 10;
+            }, 3000);
+        }
+    }, {
+        threshold: 1.0,
+    });
+
+    observer.observe(sentinel.value);
+}
+
+onMounted(() => {
+    setUpObserver();
+});
+
+onUnmounted(() => {
+    observer?.disconnect();
+});
 
 </script>
 
@@ -17,6 +47,7 @@ const count = ref<number>(10);
             <div v-for="i in count" :key="i" class="mb-4">
                 <FriendCard />
             </div>
+            <div ref="sentinel" class="h-10"></div>
         </div>
     </div>
 </template>
